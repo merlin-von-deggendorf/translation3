ae4b304a462c1192c6710b29d88b3d5b826e4e72
David <david.maruhn@stud.th-deg.de>
2025-01-10 21:32:12 +0100

    Comment out training loop and model initialization in main.py for working on data generation

--- main.py
+++ main.py
@@ -25,52 +25,52 @@ pers=Persistence('deu','eng')
 spf:StringPairsFile=pers.get_sentece_pairs()
 spf.load(10)
 spf.generate_dict(3)
-dataset=TranslationDataset(spf)
-dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, collate_fn=dataset.collate_fn)
-print(spf.languages[0].word_dict.__len__())
+# dataset=TranslationDataset(spf)
+# dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, collate_fn=dataset.collate_fn)
+# print(spf.languages[0].word_dict.__len__())
 
-OUTPUT_SIZE=spf.languages[1].word_dict.__len__()
-INPUT_SIZE=spf.languages[0].word_dict.__len__()
+# OUTPUT_SIZE=spf.languages[1].word_dict.__len__()
+# INPUT_SIZE=spf.languages[0].word_dict.__len__()
 
-encoder=Encoder(INPUT_SIZE,EMBED_SIZE,HIDDEN_SIZE,1).to(device)
-decoder=Decoder(OUTPUT_SIZE,EMBED_SIZE,HIDDEN_SIZE,spf.languages[1].pad_tokken,1).to(device)
-model=Seq2Seq(encoder,decoder,device).to(device)
+# encoder=Encoder(INPUT_SIZE,EMBED_SIZE,HIDDEN_SIZE,1).to(device)
+# decoder=Decoder(OUTPUT_SIZE,EMBED_SIZE,HIDDEN_SIZE,spf.languages[1].pad_tokken,1).to(device)
+# model=Seq2Seq(encoder,decoder,device).to(device)
 
 
-criterion = nn.CrossEntropyLoss(ignore_index=spf.languages[1].pad_tokken)  # Ignore padding index
-optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
-# calculate number of batches
-batch_count = len(dataloader)
-# Training
-for epoch in range(NUM_EPOCHS):
-    model.train()
-    epoch_loss = 0
-    batch_nr=0
-    for batch_idx, (src_batch, tgt_batch, src_lengths) in enumerate(dataloader):
-        src_batch = src_batch.to(device)
-        tgt_batch = tgt_batch.to(device)
+# criterion = nn.CrossEntropyLoss(ignore_index=spf.languages[1].pad_tokken)  # Ignore padding index
+# optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+# # calculate number of batches
+# batch_count = len(dataloader)
+# # Training
+# for epoch in range(NUM_EPOCHS):
+#     model.train()
+#     epoch_loss = 0
+#     batch_nr=0
+#     for batch_idx, (src_batch, tgt_batch, src_lengths) in enumerate(dataloader):
+#         src_batch = src_batch.to(device)
+#         tgt_batch = tgt_batch.to(device)
 
-        optimizer.zero_grad()
+#         optimizer.zero_grad()
 
-        # Forward pass
-        outputs = model(src_batch, src_lengths, tgt_batch, TEACHER_FORCE_RATIO)
-        # outputs: [batch_size, trg_len, output_dim]
-        # tgt_batch: [batch_size, trg_len]
+#         # Forward pass
+#         outputs = model(src_batch, src_lengths, tgt_batch, TEACHER_FORCE_RATIO)
+#         # outputs: [batch_size, trg_len, output_dim]
+#         # tgt_batch: [batch_size, trg_len]
 
-        # Reshape for loss computation
-        outputs = outputs[:,1:].reshape(-1, OUTPUT_SIZE)
-        trg = tgt_batch[:,1:].reshape(-1)
+#         # Reshape for loss computation
+#         outputs = outputs[:,1:].reshape(-1, OUTPUT_SIZE)
+#         trg = tgt_batch[:,1:].reshape(-1)
 
-        loss = criterion(outputs, trg)
-        loss.backward()
+#         loss = criterion(outputs, trg)
+#         loss.backward()
 
-        optimizer.step()
+#         optimizer.step()
 
-        epoch_loss += loss.item()
-        batch_nr+=1
-        print(f"Batch [{batch_nr}/{batch_count}], Loss: {loss.item():.4f}")
+#         epoch_loss += loss.item()
+#         batch_nr+=1
+#         print(f"Batch [{batch_nr}/{batch_count}], Loss: {loss.item():.4f}")
 
-    avg_loss = epoch_loss / len(dataloader)
-    print(f"Epoch [{epoch+1}/{NUM_EPOCHS}], Loss: {avg_loss:.4f}")
+#     avg_loss = epoch_loss / len(dataloader)
+#     print(f"Epoch [{epoch+1}/{NUM_EPOCHS}], Loss: {avg_loss:.4f}")
 
     
